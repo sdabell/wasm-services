daf1319d7355ada4276ff7307433285b02dc7d40
steve <sdabell@yahoo.com>
2020-06-23 07:19:37 -0700

    added diagram

--- src/run.sh
+++ src/run.sh
@@ -7,4 +7,13 @@
 
 emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_add', '_sub', '_mult', '_div']" nanoServices/arithmeticServices.c -o nanoServices/arithmeticServices.wasm
 
+emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_add']" nanoServices/addService.c -o nanoServices/addService.wasm
+
+emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_sub']" nanoServices/subService.c -o nanoServices/subService.wasm
+
+emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_mult']" nanoServices/multService.c -o nanoServices/multService.wasm
+
+emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_div']" nanoServices/divService.c -o nanoServices/divService.wasm
+
+
 #emcc -s WASM=1 -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 –s EXPORTED_FUNCTIONS="['_add', '_sub', '_mult', '_div']"   nanoServices/arithmeticServices.c
